c8ada41ed43c2165d13c695074441bcb9f2855f4
Skkeem <skkeem@gmail.com>
2015-01-16 01:04:18 +0900

    Chat actionbar hide

---
+++ src/com/appsrox/instachat/client/Constants.java
@@ -0,0 +1,18 @@
+package com.appsrox.instachat.client;
+
+/**
+ * @author appsrox.com
+ *
+ */
+public interface Constants {
+
+    /**
+     * Base URL of the Demo Server (such as http://my_host:8080/gcm-demo)
+     */
+	String SERVER_URL = "http://ingenui.iptime.org/gcm.php";
+
+    /**
+     * Google API project id registered to use GCM.
+     */
+	String SENDER_ID = "352935819908";
+}
